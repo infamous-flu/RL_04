500940c8d0ea138872e7334a40bb22b310372581
infamous-flu <s3590712@vuw.leidenuniv.nl>
2024-05-06 11:30:24 +0200

    minor formatting

--- main.py
+++ main.py
@@ -6,18 +6,17 @@ from agents.ppo import PPO
 from config import DQNConfig, PPOConfig
 
 
-def main(agent_type):
-    env_id = 'LunarLander-v2'
-    env = gym.make(env_id)
+def main(agent_type: str):
+    env = gym.make('LunarLander-v2')
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     seed = 69420
 
     match agent_type:
         case 'dqn':
-            config = DQNConfig(env_id=env_id)
+            config = DQNConfig()
             agent = DQN(env, device, config, seed)
         case 'ppo':
-            config = PPOConfig(env_id=env_id)
+            config = PPOConfig()
             agent = PPO(env, device, config, seed)
 
     agent.train(1e7)
